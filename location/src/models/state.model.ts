23354c70dc73704eecefc29fe0085fe4854a06b4
Shivesh Tiwari <stshandilya0820@gmail.com>
2022-08-01 12:20:42 +0530

    added state, integrated mondodb

---
+++ location/src/models/state.model.ts
@@ -0,0 +1,33 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class State extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'number',
+  })
+  cityCount?: number;
+
+
+  constructor(data?: Partial<State>) {
+    super(data);
+  }
+}
+
+export interface StateRelations {
+  // describe navigational properties here
+}
+
+export type StateWithRelations = State & StateRelations;
